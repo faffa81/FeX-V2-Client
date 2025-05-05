13d551bb30b4195a96b54cf4d680893fd49058e0
Dava Baba <davidbartmann22@gmail.com>
2025-05-05 17:40:01 +0200

    Teefusion support

--- src/game/client/components/fex/fexversion.h
+++ src/game/client/components/fex/fexversion.h
@@ -1,12 +1,12 @@
 #ifndef FEX_VERSION_H
 #define FEX_VERSION_H
 #ifndef FEX_RELEASE_VERSION
-#define FEX_RELEASE_VERSION "6.0"
+#define FEX_RELEASE_VERSION "6.1"
 #endif
 
 // TeeFusion
 #define FEX_BUILD_DATE __DATE__ ", " __TIME__
 
-#define FEX_VERSION_NUMBER 6000
+#define FEX_VERSION_NUMBER 6010
 #define CLIENT_NAME "FeX"
 #endif
