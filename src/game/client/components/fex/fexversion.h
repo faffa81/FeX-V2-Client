880de920063c25077c96d5e952dba4c71556f87b
Dava Baba <davidbartmann22@gmail.com>
2025-05-09 20:08:42 +0200

    dumby faffa81 forgot 6.2

--- src/game/client/components/fex/fexversion.h
+++ src/game/client/components/fex/fexversion.h
@@ -1,12 +1,12 @@
 #ifndef FEX_VERSION_H
 #define FEX_VERSION_H
 #ifndef FEX_RELEASE_VERSION
-#define FEX_RELEASE_VERSION "6.1"
+#define FEX_RELEASE_VERSION "6.2"
 #endif
 
 // TeeFusion
 #define FEX_BUILD_DATE __DATE__ ", " __TIME__
 
-#define FEX_VERSION_NUMBER 6010
+#define FEX_VERSION_NUMBER 6020
 #define CLIENT_NAME "FeX"
 #endif
